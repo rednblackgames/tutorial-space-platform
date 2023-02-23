849f17e31a297c93d4462789e1a22d462601ad83
fgnm <fgnm.dev@gmail.com>
2023-02-23 21:37:03 +0100

    Draft switch-gdx project

---
+++ switch/src/main/java/games/rednblack/hyperrunner/switchgdx/SwitchLauncher.java
@@ -0,0 +1,12 @@
+package games.rednblack.hyperrunner.switchgdx;
+
+import com.thelogicmaster.switchgdx.SwitchApplication;
+
+import games.rednblack.hyperrunner.HyperRunner;
+
+public class SwitchLauncher {
+
+	public static void main (String[] arg) {
+		new SwitchApplication(new HyperRunner());
+	}
+}
